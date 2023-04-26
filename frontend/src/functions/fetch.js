369cffe02301047c0de292352b0314eee20392cc
Philippovitsch <philipp@stadter.at>
2023-04-26 13:55:00 +0200

    frontend: Refactor fetch functions to remove boilerplate code

--- frontend/src/functions/fetch.js
+++ frontend/src/functions/fetch.js
@@ -15,77 +15,47 @@ function getAxiosConfig(subDomain, requestMethod, token, requestBody) {
   return config;
 }
 
+function fetchData(axiosConfig, token) {
+  if (token === "Bearer null") {
+    return { status: -1 }
+  }
+
+  return axiosInstance(axiosConfig)
+    .then(response => {
+      return response;
+    })
+    .catch(error => {
+      console.log("Error: " + error);
+      return error.response;
+    });
+}
+
 export function getBearerToken(encodedToken) {
   const token = "Basic " + encodedToken;
   const axiosConfig = getAxiosConfig("/api/auth/authenticate", "POST", token, {});
-  return axiosInstance(axiosConfig)
-    .then(response => {
-      return response;
-    })
-    .catch(error => {
-      console.log("Error: " + error);
-      return error.response;
-    });
+  return fetchData(axiosConfig, token);
 }
 
 export function signUp(userData) {
-  return axiosInstance.post(url + "/api/auth/sign-up", userData)
-    .then(response => {
-      return response;
-    })
-    .catch(error => {
-      console.log("Error: " + error);
-      return error.response;
-    })
+  const token = ""
+  const axiosConfig = getAxiosConfig("/api/auth/sign-up", "POST", token, userData);
+  return fetchData(axiosConfig, token);
 }
 
 export function getAllTasks() {
-  if (localStorage.getItem("bearerToken") === null) {
-    return { status: -1 }
-  }
-
   const token = "Bearer " + localStorage.getItem("bearerToken");
   const axiosConfig = getAxiosConfig("/api/tasks/all", "GET", token, {});
-  return axiosInstance(axiosConfig)
-    .then(response => {
-      return response;
-    })
-    .catch(error => {
-      console.log("Error: " + error);
-      return error.response;
-    });
+  return fetchData(axiosConfig, token);
 }
 
 export function saveNewTask(task) {
-  if (localStorage.getItem("bearerToken") === null) {
-    return { status: -1 }
-  }
-
   const token = "Bearer " + localStorage.getItem("bearerToken");
   const axiosConfig = getAxiosConfig("/api/tasks", "POST", token, task);
-  return axiosInstance(axiosConfig)
-    .then(response => {
-      return response;
-    })
-    .catch(error => {
-      console.log("Error: " + error);
-      return error.response;
-    });
+  return fetchData(axiosConfig, token);
 }
 
 export function deleteTaskByName(name) {
-  if (localStorage.getItem("bearerToken") === null) {
-    return { status: -1 }
-  }
-
   const token = "Bearer " + localStorage.getItem("bearerToken");
   const axiosConfig = getAxiosConfig(`/api/tasks/${name}`, "DELETE", token, {});
-  return axiosInstance(axiosConfig)
-    .then(response => {
-      return response;
-    })
-    .catch(error => {
-      console.log("Error: " + error);
-      return error.response;
-    });
+  return fetchData(axiosConfig, token);
 }
