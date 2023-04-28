1a1427be529c887f39c979f3df911a5e22e0b7fa
Philippovitsch <philipp@stadter.at>
2023-04-28 19:42:12 +0200

    backend: Implement Lombok and clean up database relationships

--- backend/src/main/java/com/codecool/tasktracker/model/Tag.java
+++ backend/src/main/java/com/codecool/tasktracker/model/Tag.java
@@ -1,40 +1,22 @@
 package com.codecool.tasktracker.model;
 
 import jakarta.persistence.*;
-
-import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tag {
-
-    public Tag() {}
-
-    public Tag(String name, Set<Task> tasks) {
-        this.name = name;
-        this.tasks = tasks;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
-    @ManyToMany(mappedBy = "tags")
-    private Set<Task> tasks;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
--- backend/src/main/java/com/codecool/tasktracker/model/Task.java
+++ backend/src/main/java/com/codecool/tasktracker/model/Task.java
@@ -1,28 +1,29 @@
 package com.codecool.tasktracker.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
 import java.util.Set;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
-
-    public Task() {}
-
-    public Task(String name, String description, Timestamp timestamp, Set<Tag> tags) {
-        this.name = name;
-        this.description = description;
-        this.timestamp = timestamp;
-        this.tags = tags;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
     private String description;
     private Timestamp timestamp;
+
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "task_tag",
@@ -30,41 +31,5 @@ public class Task {
             inverseJoinColumns = @JoinColumn(name = "tag_id")
     )
     private Set<Tag> tags;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Timestamp getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Timestamp timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public Set<Tag> getTags() {
-        return tags;
-    }
 
 }
