9808208f9f7e3d46dc9d6c2e39011d08a5ca052d
Philippovitsch <philipp@stadter.at>
2023-06-12 22:46:58 +0200

    frontend: Implement possibility to switch between user tasks and all tasks

--- frontend/src/components/Tasks.tsx
+++ frontend/src/components/Tasks.tsx
@@ -4,14 +4,18 @@ import useTasks from "../logic/TaskLogic";
 import { Link } from "react-router-dom";
 
 export default function Tasks() {
-  const [{ tasks, deleteTask }] = useTasks(); 
+  const [{ tasks, triggerUserTasks, deleteTask }] = useTasks();
 
   return (
     <Container text style={{ paddingTop: "100px", paddingBottom: "100px" }}>
       <Header as="h2" style={{ marginBottom: "25px" }}>Saved Tasks</Header>
       { !localStorage.getItem("user")
         ? <p> Please <Link to="/login">login</Link> or <Link to="/sign-up">sign up</Link> to see the tasks...</p>
-        : tasks.map(task => <Task key={ task.id } task={ task } deleteTask={ deleteTask } />)
+        : <>
+          <span className="task-option" onClick={() => triggerUserTasks(false)}>All Tasks</span>
+          <span className="task-option" onClick={() => triggerUserTasks(true)}>Your tasks</span>
+          { tasks.map(task => <Task key={ task.id } task={ task } deleteTask={ deleteTask } />) }
+        </>
       }
     </Container>
   );
--- frontend/src/logic/TaskLogic.ts
+++ frontend/src/logic/TaskLogic.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { deleteTaskByName, getAllTasks } from "../functions/fetch";
+import {deleteTaskByName, getAllTasks, getTasksByUsername} from "../functions/fetch";
 import { TaskModel } from "../model/TaskModel";
 
 const useTasks = () => {
@@ -16,6 +16,21 @@ const useTasks = () => {
     }
   };
 
+  const getUserTasks = async () => {
+    const response = await getTasksByUsername();
+    if (response.status === 200) {
+      setTasks((response.data));
+    }
+  }
+
+  const triggerUserTasks = (triggerUserTasks = false) => {
+    if (triggerUserTasks) {
+      getUserTasks();
+    } else {
+      getTasks();
+    }
+  };
+
   const deleteTask = async (task: TaskModel) => {
     const name = task.name;
     const response = await deleteTaskByName(name);
@@ -25,7 +40,7 @@ const useTasks = () => {
     }
   };
 
-  return [{ tasks, deleteTask }];
+  return [{ tasks, triggerUserTasks, deleteTask }];
 };
 
 export default useTasks;
