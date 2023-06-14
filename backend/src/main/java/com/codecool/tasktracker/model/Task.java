6129abedcb4ad76c2343c09924a822bceeeb5c98
Philippovitsch <philipp@stadter.at>
2023-06-14 11:38:21 +0200

    backend: Remove unused "Builder" annotations and replace some getters and setters with "Data" annotation

--- backend/src/main/java/com/codecool/tasktracker/model/Task.java
+++ backend/src/main/java/com/codecool/tasktracker/model/Task.java
@@ -2,7 +2,6 @@ package com.codecool.tasktracker.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,7 +10,6 @@ import java.util.Set;
 
 @Entity
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Task {
