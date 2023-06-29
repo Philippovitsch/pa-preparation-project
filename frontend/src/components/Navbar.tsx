8accce2c4027196a722c7901c762c49cda307b0d
Philippovitsch <philipp@stadter.at>
2023-06-29 09:17:35 +0200

    frontend: Move Login/ Logout/ Registration button to the right side of the page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -13,20 +13,19 @@ export default function Navbar(props:
           <Image size="mini" src="/icon.png" style={{ marginRight: "1.5em" }} />
           Task Tracker
         </Menu.Item>
-        { props.user
-          ? <Menu.Item as={Link} to="/logout">Logout</Menu.Item>
-          : <>
-            <Menu.Item as={Link} to="/login">Login</Menu.Item>
-            <Menu.Item as={Link} to="/sign-up">Sign Up</Menu.Item>
-          </>
-        }
         <Menu.Item as={Link} to="/new-task">Add new task</Menu.Item>
         <div className='login-status'>
-          { !props.user
-            ? <i>Not logged in</i>
-            : <i>Logged in as: <Link to="/user"><b>{ props.user }</b></Link></i>
+          { props.user
+            ? <>
+              <i>Logged in as: <Link to="/user"><b>{ props.user }</b></Link></i>
+              <Menu.Item as={Link} to="/logout">Logout</Menu.Item>
+            </>
+            : <>
+              <Menu.Item as={Link} to="/sign-up">Sign Up</Menu.Item>
+              <Menu.Item as={Link} to="/login">Login</Menu.Item>
+            </>
           }
-      </div>
+        </div>
       </Container>
     </Menu>
   );
